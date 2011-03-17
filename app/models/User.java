83de3e6df0459fa1ce4cbc3e13909815ba045d26
Parag Shah <adamin@adaptivelearningonline.net>
2011-03-17 11:20:55 +0530

    added model objects

---
+++ app/models/User.java
@@ -0,0 +1,68 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class User extends Model {
+	
+	public String email;
+	public String password;
+	public String fullName;
+	public boolean isAdmin;
+	
+	public User(String email, String password, String fullName) {
+		this.email = email;
+		this.password = password;
+		this.fullName = fullName;
+	}
+
+	public static User connect(String email, String password) {
+		return User.find("byEmailAndPassword", email, password).first();
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((fullName == null) ? 0 : fullName.hashCode());
+		result = prime * result + (isAdmin ? 1231 : 1237);
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (fullName == null) {
+			if (other.fullName != null)
+				return false;
+		} else if (!fullName.equals(other.fullName))
+			return false;
+		if (isAdmin != other.isAdmin)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	
+	
+}
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,104 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
+	@Before
+	public void setUp() {
+		Fixtures.deleteAll();
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void testCreateAndRetrieveUser() {
+    	User bobTheUser = new User("bob@bobbobbob.net", "bobspwd", "Bob The User");
+    	bobTheUser.save();
+    	
+    	User retrievedBob = User.find("byEmail", "bob@bobbobbob.net").first();
+    	
+    	//tests
+    	assertNotNull(retrievedBob);
+    	assertEquals(bobTheUser, retrievedBob);
+    }
+    
+    @Test
+    public void testConnectUser() {
+    	User bobTheUser = new User("bob@bobbobbob.net", "bobspwd", "Bob The User");
+    	bobTheUser.save();
+    	
+    	assertNotNull(User.connect("bob@bobbobbob.net", "bobspwd"));    	
+    	assertNull(User.connect("bob@bobbobbob.net", "bobswrongpwd"));
+    	assertNull(User.connect("tom@tomtomtom.net", "tomsdoestnhaveanaccount"));
+    }
+    
+    @Test
+    public void testCreateAndRetrievePost() {
+    	User bobTheUser = new User("bob@bobbobbob.net", "bobspwd", "Bob The User");
+    	bobTheUser.save();
+    	
+    	Post post = new Post(bobTheUser, "Play is awesome", "Play is an awesome framework");
+    	post.save();
+    	
+    	//tests
+    	assertEquals(1, Post.count());
+    	List<Post> bobPosts = Post.find("byAuthor", bobTheUser).fetch();
+    	assertEquals(1, bobPosts.size());
+    	Post firstPost = bobPosts.get(0);
+    	assertEquals(post.title, firstPost.title);
+    	assertEquals(post.content, firstPost.content);
+    	assertEquals(post.author, firstPost.author);
+    	assertEquals(post.postedAt, firstPost.postedAt);
+    }
+    
+    @Test
+    public void testCreateAndRetrieveComments() {
+    	User bobTheUser = new User("bob@bobbobbob.net", "bobspwd", "Bob The User");
+    	bobTheUser.save();
+    	
+    	Post post = new Post(bobTheUser, "Play is awesome", "Play is an awesome framework");
+    	post.save();
+    	
+    	Comment comment = new Comment("Bob", 
+    								  "This is my wonderful and insightful comment", 
+    								  post);
+    	comment.save();
+    	
+    	List<Comment> retrievedComments = Comment.find("byAuthor", "Bob").fetch();
+    	assertEquals(1, retrievedComments.size());
+    	Comment retrievedComment = retrievedComments.get(0);
+    	assertNotNull(retrievedComment);
+    	assertEquals(comment, retrievedComment);
+    }
+    
+    @Test
+    public void testAddAndDeleteComment() {
+    	User bobTheUser = new User("bob@bobbobbob.net", "bobspwd", "Bob The User");
+    	bobTheUser.save();
+    	
+    	Post post = new Post(bobTheUser, "Play is awesome", "Play is an awesome framework");
+    	post.save();
+    	
+    	post.
+    		addComment("Bob", "This is Bob's comment").
+    			addComment("Tom", "This is another comment");
+    	
+    	List<Comment> comments = Comment.find("byPost", post).fetch();
+    	
+    	assertEquals(2, comments.size());
+    	assertEquals("Bob", comments.get(0).author);
+    	assertEquals("This is Bob's comment", comments.get(0).content);
+    	assertEquals(post, comments.get(0).post);
+    	assertEquals("Tom", comments.get(1).author);
+    	assertEquals("This is another comment", comments.get(1).content);
+    	assertEquals(post, comments.get(1).post);
+    	
+    	post.delete();
+    	
+    	assertEquals(1, User.count());
+    	assertEquals(0, Post.count());
+    	assertEquals(0, Comment.count());
     }
 
 }
