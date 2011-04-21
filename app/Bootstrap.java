09008b201cfc0eba718a49f07cd7bff0e21890b1
Parag Shah <adaptives@gmail.com>
2011-04-21 19:12:38 +0530

    when a Play app is changed on the fly it seems to invoke this class when the next url is recieved. This was causing a constraint violation. Fixed that error

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,3 +1,4 @@
+import models.SocialUser;
 import models.User;
 import play.Play;
 import play.jobs.Job;
@@ -8,7 +9,7 @@ import play.test.Fixtures;
 public class Bootstrap extends Job {
 
 	public void doJob() {
-		if(Play.mode == Play.Mode.DEV) {
+		if(Play.mode == Play.Mode.DEV && SocialUser.findAll().size() == 0) {
 			Fixtures.load("users-and-study-sessions.yml");
 			Fixtures.load("pages.yml");
 			Fixtures.load("diycourses.yml");
