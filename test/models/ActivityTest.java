fc68a94bb1f4695612a1f606da4ccc64de25a8fb
Parag Shah <adaptives@gmail.com>
2011-05-04 20:19:34 +0530

    added methods for getting study session and session part from an Activity object

---
+++ test/models/ActivityTest.java
@@ -0,0 +1,20 @@
+package models;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ActivityTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+}
