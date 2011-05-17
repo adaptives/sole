b0f2b93159dd77480bfc7c3bea9764220571b1a5
Parag Shah <adaptives@gmail.com>
2011-05-17 13:33:59 +0530

    Made the affiliate code field a Lob and gave it a size

--- app/models/AffiliateCode.java
+++ app/models/AffiliateCode.java
@@ -1,12 +1,16 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 
+import play.data.validation.MaxSize;
 import play.db.jpa.Model;
 
 @Entity
 public class AffiliateCode extends Model {
 	
+	@Lob
+	@MaxSize(2048)
 	public String code;
 	
 	public AffiliateCode(String code) {
