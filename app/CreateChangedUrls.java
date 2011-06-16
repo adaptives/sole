d9fcd80cd8ce5d1784042d0a2fe78193b6ee9b72
Parag Shah <adaptives@gmail.com>
2011-06-16 18:06:32 +0530

    Commented out code which added new url's for study sessions

--- app/CreateChangedUrls.java
+++ app/CreateChangedUrls.java
@@ -20,47 +20,47 @@ import play.test.Fixtures;
 public class CreateChangedUrls extends Job {
 	
 	public void doJob() {
-		List<StudySession> studySessions = StudySession.findAll();		
-		
-		for(StudySession studySession : studySessions) {
-			
-			// Link to show question
-			for(Question question : studySession.forum.questions) {
-				Map<String, Object> showStudySessionDiscussionQuestionArgs = new HashMap<String, Object>();
-				showStudySessionDiscussionQuestionArgs.put("sanitizedTitle", studySession.sanitizedTitle);
-				showStudySessionDiscussionQuestionArgs.put("questionId", question.id);
-				showStudySessionDiscussionQuestionArgs.put("sanitizedQuestionTitle", question.sanitizedTitle);
-				String newForumQuestionUrl = Router.reverse("StudySessionC.forumQuestion", showStudySessionDiscussionQuestionArgs).url;
-				String oldForumQuestionUrl = "/studysessions/studysession/" + studySession.id + "/forum/question/" + question.id;
-				ChangedUrl forumQuestionChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldForumQuestionUrl).first();
-				forumQuestionChangedUrl.newUrl = newForumQuestionUrl;
-				forumQuestionChangedUrl.save();
-			}
-			
-			for(SessionPart sessionPart : studySession.sessionParts) {
-				//sessionPart(String sanitizedTitle, String sessionPartSanitizedTitle)
-				
-				// Link to show a session part
-				Map<String, Object> showSessionPartArgs = new HashMap<String, Object>();
-				showSessionPartArgs.put("sanitizedTitle", studySession.sanitizedTitle);
-				showSessionPartArgs.put("sessionPartSanitizedTitle", sessionPart.sanitizedTitle);
-				String newSessionPartUrl = Router.reverse("StudySessionC.sessionPart", showSessionPartArgs).url;
-				String oldSessionPartUrl = "/studysessions/studysession/" + studySession.id + "/sessionpart/" + sessionPart.id;
-				ChangedUrl sessionPartChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldSessionPartUrl).first();
-				sessionPartChangedUrl.newUrl = newSessionPartUrl;
-				sessionPartChangedUrl.save();
-				
-				// View all activity responses of a session sessionPartActivityResponses(String sanitizedTitle, String sessionPartSanitizedTitle)
-				Map<String, Object> showSessionPartActivityResponsesArgs = new HashMap<String, Object>();
-				showSessionPartActivityResponsesArgs.put("sanitizedTitle", studySession.sanitizedTitle);
-				showSessionPartActivityResponsesArgs.put("sessionPartSanitizedTitle", sessionPart.sanitizedTitle);
-				String newSessionPartActivityResponseUrl = Router.reverse("StudySessionC.sessionPartActivityResponses", showSessionPartActivityResponsesArgs).url;
-				String oldSessionPartActivityResponseUrl = "/studysessions/studysession/" + studySession.id + "/sessionpart/" + sessionPart.id + "/activity/responses";
-				ChangedUrl sessionPartActivityResponseChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldSessionPartActivityResponseUrl).first();
-				sessionPartActivityResponseChangedUrl.newUrl = newSessionPartActivityResponseUrl;
-				sessionPartActivityResponseChangedUrl.save();
-			}
-			
-		}
+//		List<StudySession> studySessions = StudySession.findAll();		
+//		
+//		for(StudySession studySession : studySessions) {
+//			
+//			// Link to show question
+//			for(Question question : studySession.forum.questions) {
+//				Map<String, Object> showStudySessionDiscussionQuestionArgs = new HashMap<String, Object>();
+//				showStudySessionDiscussionQuestionArgs.put("sanitizedTitle", studySession.sanitizedTitle);
+//				showStudySessionDiscussionQuestionArgs.put("questionId", question.id);
+//				showStudySessionDiscussionQuestionArgs.put("sanitizedQuestionTitle", question.sanitizedTitle);
+//				String newForumQuestionUrl = Router.reverse("StudySessionC.forumQuestion", showStudySessionDiscussionQuestionArgs).url;
+//				String oldForumQuestionUrl = "/studysessions/studysession/" + studySession.id + "/forum/question/" + question.id;
+//				ChangedUrl forumQuestionChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldForumQuestionUrl).first();
+//				forumQuestionChangedUrl.newUrl = newForumQuestionUrl;
+//				forumQuestionChangedUrl.save();
+//			}
+//			
+//			for(SessionPart sessionPart : studySession.sessionParts) {
+//				//sessionPart(String sanitizedTitle, String sessionPartSanitizedTitle)
+//				
+//				// Link to show a session part
+//				Map<String, Object> showSessionPartArgs = new HashMap<String, Object>();
+//				showSessionPartArgs.put("sanitizedTitle", studySession.sanitizedTitle);
+//				showSessionPartArgs.put("sessionPartSanitizedTitle", sessionPart.sanitizedTitle);
+//				String newSessionPartUrl = Router.reverse("StudySessionC.sessionPart", showSessionPartArgs).url;
+//				String oldSessionPartUrl = "/studysessions/studysession/" + studySession.id + "/sessionpart/" + sessionPart.id;
+//				ChangedUrl sessionPartChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldSessionPartUrl).first();
+//				sessionPartChangedUrl.newUrl = newSessionPartUrl;
+//				sessionPartChangedUrl.save();
+//				
+//				// View all activity responses of a session sessionPartActivityResponses(String sanitizedTitle, String sessionPartSanitizedTitle)
+//				Map<String, Object> showSessionPartActivityResponsesArgs = new HashMap<String, Object>();
+//				showSessionPartActivityResponsesArgs.put("sanitizedTitle", studySession.sanitizedTitle);
+//				showSessionPartActivityResponsesArgs.put("sessionPartSanitizedTitle", sessionPart.sanitizedTitle);
+//				String newSessionPartActivityResponseUrl = Router.reverse("StudySessionC.sessionPartActivityResponses", showSessionPartActivityResponsesArgs).url;
+//				String oldSessionPartActivityResponseUrl = "/studysessions/studysession/" + studySession.id + "/sessionpart/" + sessionPart.id + "/activity/responses";
+//				ChangedUrl sessionPartActivityResponseChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldSessionPartActivityResponseUrl).first();
+//				sessionPartActivityResponseChangedUrl.newUrl = newSessionPartActivityResponseUrl;
+//				sessionPartActivityResponseChangedUrl.save();
+//			}
+//			
+//		}
 	}
 }
