682372b06c136b30633765f7b41b62c5b06315fa
Parag Shah <adaptives@gmail.com>
2011-06-16 17:41:06 +0530

    Adding new urls for all studysession old url's

--- app/CreateChangedUrls.java
+++ app/CreateChangedUrls.java
@@ -20,52 +20,47 @@ import play.test.Fixtures;
 public class CreateChangedUrls extends Job {
 	
 	public void doJob() {
-//		List<StudySession> studySessions = StudySession.findAll();		
-//		
-//		for(StudySession studySession : studySessions) {
-//			// Link to show the study session
-//			Map<String, Object> showStudySessionArgs = new HashMap<String, Object>();
-//			showStudySessionArgs.put("id", studySession.id);
-//			new ChangedUrl(Router.reverse("StudySessionC.studySession", showStudySessionArgs).url, "");	
-//			
-//			// Link to show participants
-//			Map<String, Object> showStudySessionParticipantsArgs = new HashMap<String, Object>();
-//			showStudySessionParticipantsArgs.put("studySessionId", studySession.id);
-//			new ChangedUrl(Router.reverse("StudySessionC.participants", showStudySessionParticipantsArgs).url, "");
-//			
-//			// Link to show resources
-////			Map<String, Object> showStudySessionResourcesArgs = new HashMap<String, Object>();
-////			showStudySessionResourcesArgs.put("studySessionId", studySession.id);
-////			System.out.println(Router.reverse("StudySessionC.resources", showStudySessionResourcesArgs).url);
-//			
-//			// Link to show discussion forum
-//			Map<String, Object> showStudySessionDiscussioArgs = new HashMap<String, Object>();
-//			showStudySessionDiscussioArgs.put("studySessionId", studySession.id);
-//			new ChangedUrl(Router.reverse("StudySessionC.forum", showStudySessionDiscussioArgs).url, "");
-//			
-//			// Link to show question
-//			for(Question question : studySession.forum.questions) {
-//				Map<String, Object> showStudySessionDiscussionQuestionArgs = new HashMap<String, Object>();
-//				showStudySessionDiscussionQuestionArgs.put("studySessionId", studySession.id);
-//				showStudySessionDiscussionQuestionArgs.put("questionId", question.id);
-//				new ChangedUrl(Router.reverse("StudySessionC.forumQuestion", showStudySessionDiscussionQuestionArgs).url, "");
-//			}
-//			
-//			
-//			for(SessionPart sessionPart : studySession.sessionParts) {
-//				// Link to show a session part
-//				Map<String, Object> showSessionPartArgs = new HashMap<String, Object>();
-//				showSessionPartArgs.put("studySessionId", studySession.id);
-//				showSessionPartArgs.put("id", sessionPart.id);
-//				new ChangedUrl(Router.reverse("StudySessionC.sessionPart", showSessionPartArgs).url, "");
-//				
-//				// View all activity responses of a session sessionPartActivityResponses(long studySessionId, long sessionPartId) {
-//				Map<String, Object> showSessionPartActivityResponsesArgs = new HashMap<String, Object>();
-//				showSessionPartActivityResponsesArgs.put("studySessionId", studySession.id);
-//				showSessionPartActivityResponsesArgs.put("sessionPartId", sessionPart.id);
-//				new ChangedUrl(Router.reverse("StudySessionC.sessionPartActivityResponses", showSessionPartActivityResponsesArgs).url, "");
-//			}
-//			
-//		}
+		List<StudySession> studySessions = StudySession.findAll();		
+		
+		for(StudySession studySession : studySessions) {
+			
+			// Link to show question
+			for(Question question : studySession.forum.questions) {
+				Map<String, Object> showStudySessionDiscussionQuestionArgs = new HashMap<String, Object>();
+				showStudySessionDiscussionQuestionArgs.put("sanitizedTitle", studySession.sanitizedTitle);
+				showStudySessionDiscussionQuestionArgs.put("questionId", question.id);
+				showStudySessionDiscussionQuestionArgs.put("sanitizedQuestionTitle", question.sanitizedTitle);
+				String newForumQuestionUrl = Router.reverse("StudySessionC.forumQuestion", showStudySessionDiscussionQuestionArgs).url;
+				String oldForumQuestionUrl = "/studysessions/studysession/" + studySession.id + "/forum/question/" + question.id;
+				ChangedUrl forumQuestionChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldForumQuestionUrl).first();
+				forumQuestionChangedUrl.newUrl = newForumQuestionUrl;
+				forumQuestionChangedUrl.save();
+			}
+			
+			for(SessionPart sessionPart : studySession.sessionParts) {
+				//sessionPart(String sanitizedTitle, String sessionPartSanitizedTitle)
+				
+				// Link to show a session part
+				Map<String, Object> showSessionPartArgs = new HashMap<String, Object>();
+				showSessionPartArgs.put("sanitizedTitle", studySession.sanitizedTitle);
+				showSessionPartArgs.put("sessionPartSanitizedTitle", sessionPart.sanitizedTitle);
+				String newSessionPartUrl = Router.reverse("StudySessionC.sessionPart", showSessionPartArgs).url;
+				String oldSessionPartUrl = "/studysessions/studysession/" + studySession.id + "/sessionpart/" + sessionPart.id;
+				ChangedUrl sessionPartChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldSessionPartUrl).first();
+				sessionPartChangedUrl.newUrl = newSessionPartUrl;
+				sessionPartChangedUrl.save();
+				
+				// View all activity responses of a session sessionPartActivityResponses(String sanitizedTitle, String sessionPartSanitizedTitle)
+				Map<String, Object> showSessionPartActivityResponsesArgs = new HashMap<String, Object>();
+				showSessionPartActivityResponsesArgs.put("sanitizedTitle", studySession.sanitizedTitle);
+				showSessionPartActivityResponsesArgs.put("sessionPartSanitizedTitle", sessionPart.sanitizedTitle);
+				String newSessionPartActivityResponseUrl = Router.reverse("StudySessionC.sessionPartActivityResponses", showSessionPartActivityResponsesArgs).url;
+				String oldSessionPartActivityResponseUrl = "/studysessions/studysession/" + studySession.id + "/sessionpart/" + sessionPart.id + "/activity/responses";
+				ChangedUrl sessionPartActivityResponseChangedUrl = ChangedUrl.find("select cu from ChangedUrl cu where cu.oldUrl = ?", oldSessionPartActivityResponseUrl).first();
+				sessionPartActivityResponseChangedUrl.newUrl = newSessionPartActivityResponseUrl;
+				sessionPartActivityResponseChangedUrl.save();
+			}
+			
+		}
 	}
 }
