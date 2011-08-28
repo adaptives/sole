59a8e63067115610fc5ac6e3985813be8bf74dc1
Parag <adaptives@gmail.com>
2011-08-28 21:36:40 +0530

    Added toString to ActivityResponse

--- app/models/ActivityResponse.java
+++ app/models/ActivityResponse.java
@@ -43,4 +43,39 @@ public class ActivityResponse extends Model {
 		}
 		this.timestamp = new Date();
 	}
+	
+	public void like(SocialUser user) {
+		//Users cannot 'like' their own question and they cannot upvote any 
+		//question more than once 
+		if(this.user.id != user.id) {
+			if(!hasLiked(user)) {
+				ActivityResponseLiked arl = new ActivityResponseLiked(this,user);
+				arl.save();
+			} else {
+				//TODO Rate check ?
+				//cLogger.warn(user.id + "'" + " has already liked question '" + this.id);
+			}			
+		} else {
+			//TODO Rate check ?
+			//cLogger.warn(user.id + "'" + " cannot like own question '" + this.id);
+		}
+	}
+	
+	public int likes() {
+		String query = "select count(distinct arl) from ActivityResponseLiked arl where arl.activityResponse.id = ?";
+		return (int)ActivityResponseLiked.count(query, this.id);
+	}
+	
+	private boolean hasLiked(SocialUser user) {
+		String query = "select count(distinct arl) from ActivityResponseLiked arl where arl.activityResponse.id = ? and arl.user.id = ?";
+		return (ActivityResponseLiked.count(query, this.id, user.id) > 0);
+	}
+	
+	@Override
+	public String toString() {
+		return this.user.screenname + " " + 
+		       this.activity.title + " " + 		  
+		       this.responseLink + "/" +
+		       this.title;
+	}
 }
