35b7c8cfd726a81b80bb037f61bc430db9b7a86e
Parag <adaptives@gmail.com>
2011-09-26 16:19:13 +0530

    Added a captha to the feedback form

--- app/controllers/FeedbackC.java
+++ app/controllers/FeedbackC.java
@@ -1,31 +1,37 @@
 package controllers;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import models.Feedback;
 import models.ModeratedFeedback;
 import play.data.validation.Email;
+import play.data.validation.Equals;
 import play.data.validation.Required;
 import play.mvc.Controller;
 
 public class FeedbackC extends Controller {
 
 	public static void index() {
-		List<ModeratedFeedback> feedbacks = ModeratedFeedback.findModerated();		
+		List<ModeratedFeedback> feedbacks = ModeratedFeedback.findModerated();
 		render(feedbacks);
 	}
 
 	public static void createFeedback(@Required String name,
-			@Required @Email String email, @Required String message) {
+									  @Required @Email String email, 
+									  @Required String message,
+									  String cans,
+									  @Equals(value="cans", message="must.be.human") String cansval) {
 		if (validation.hasErrors()) {
 			validation.keep();
 			params.flash();
 			flash.error("Please correct these errors");
 			index();
 		}
-		Feedback feedback = new Feedback(name, email, message);
+		ModeratedFeedback feedback = new ModeratedFeedback(name, email, message);
 		feedback.save();
-		
+		flash.success("Thank you for your feedback, it will be displayed as soon as it is approved");
 		index();
 	}
 }
