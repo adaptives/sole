8caa8b78ad71802ef315f67dbc7b024426aa349b
Parag <adaptives@gmail.com>
2011-09-26 15:21:38 +0530

    removed job to move Feedback to ModeratedFeedback

--- app/CreateCourse.java
+++ app/CreateCourse.java
@@ -14,39 +14,8 @@ import play.test.Fixtures;
 
 @OnApplicationStart
 public class CreateCourse extends Job {
-
-	private String bannedWords[] = {"viagra", "cialis", "weight", "drug", "is it yours too", "rx", "news.com", "sleep", "virility", "hot"};
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public void doJob() {
-		List<Feedback> feedbacks = Feedback.findAll();
-		for(Feedback feedback : feedbacks) {
-			boolean copy = true;
-			for(String bannedWord : bannedWords) {
-				if(feedback.message != null) {
-					String capsMessage = feedback.message.toUpperCase();
-					if(capsMessage.contains(bannedWord.toUpperCase())) {
-						copy = false;
-					}
-				}
-			}
-			if(copy) {
-				ModeratedFeedback mFeedback = new ModeratedFeedback(feedback.name, feedback.email, feedback.message);
-				mFeedback.timestamp = null;
-				mFeedback.save();
-			}
-		}
+		
 	}
 }
