03a1a1ab86000189264f4d9937baff414ce12c2e
Parag <adaptives@gmail.com>
2011-09-27 12:01:20 +0530

    Fixed NPR in ModeartedFeedback

--- app/models/ModeratedFeedback.java
+++ app/models/ModeratedFeedback.java
@@ -32,8 +32,8 @@ public class ModeratedFeedback extends Model {
 		String template = "'%s' : '%s' at '%s' approved %b '%s'";
 		return String.format(template, 
 							 this.name, 
-							 this.email, 
-							 this.timestamp.toString(), 
+							 this.email,
+							 this.timestamp != null ? this.timestamp.toString() : "", 
 							 this.moderated, 
 							 this.message);
 	}
