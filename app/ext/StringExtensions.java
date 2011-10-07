74345f4308be81d75e00020b8dafbb863092a020
Parag <adaptives@gmail.com>
2011-10-07 11:49:25 +0530

    Added String extensions

---
+++ app/ext/StringExtensions.java
@@ -0,0 +1,9 @@
+package ext;
+
+import play.templates.JavaExtensions;
+
+public class StringExtensions extends JavaExtensions {
+	public static String sp2nbsp(Object obj) {
+		return obj.toString().replaceFirst(" ", "&nbsp;");
+	}
+}
