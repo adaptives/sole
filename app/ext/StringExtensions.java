4e25fa96fb52270f41156f62fa3377594ce76421
Parag <adaptives@gmail.com>
2011-10-19 14:39:33 +0530

    added markdown capability to activity reviews

--- app/ext/StringExtensions.java
+++ app/ext/StringExtensions.java
@@ -1,9 +1,51 @@
 package ext;
 
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+
+import models.ActivityResponseReview;
+
+import org.codehaus.groovy.runtime.StringBufferWriter;
+import org.tautua.markdownpapers.Markdown;
+
+import other.utils.HtmlSanitizer;
+import play.Logger;
 import play.templates.JavaExtensions;
 
 public class StringExtensions extends JavaExtensions {
+	
+	public static final org.apache.log4j.Logger cLogger = Logger.log4j.getLogger(StringExtensions.class);
+	
 	public static String sp2nbsp(Object obj) {
 		return obj.toString().replaceAll(" ", "&nbsp;");
+	}
+	
+	public static String md(Object obj) {
+		String retVal = obj.toString();
+		try {
+			StringBuffer buff = new StringBuffer();
+			Markdown md = new Markdown();
+			Reader in = new StringReader(obj.toString());
+			Writer out = new StringBufferWriter(buff);
+			md.transform(in, out);
+			retVal = buff.toString();
+			retVal = HtmlSanitizer.clean(retVal); 
+		} catch (Exception e) {
+			String msg = "Could not parse for markdown '" + obj.toString() + "'"; 
+			cLogger.error(msg, e);
+		}
+		return retVal;
+	}
+	
+	public static String sanitize(Object obj) {
+		String retVal = obj.toString();
+		try {
+			retVal = HtmlSanitizer.clean(retVal); 
+		} catch (Exception e) {
+			String msg = "Could not sanitize '" + obj.toString() + "'"; 
+			cLogger.error(msg, e);
+		}
+		return retVal;
 	}
 }
--- app/models/ActivityResponseReview.java
+++ app/models/ActivityResponseReview.java
@@ -1,16 +1,34 @@
 package models;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.text.ParseException;
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.codehaus.groovy.runtime.StringBufferWriter;
+import org.hsqldb.lib.StringInputStream;
+import org.tautua.markdownpapers.Markdown;
+
+import other.utils.HtmlSanitizer;
+
+import controllers.CourseC;
+
+import play.Logger;
 import play.db.jpa.Model;
 
 @Entity
 public class ActivityResponseReview extends Model {
 
+	public static final org.apache.log4j.Logger cLogger = Logger.log4j.getLogger(ActivityResponseReview.class);
+	
 	@ManyToOne
 	public ActivityResponse activityResponse;
 	
@@ -31,5 +49,23 @@ public class ActivityResponseReview extends Model {
 		this.review = review;
 		this.timestamp = new Date();
 		create();
+	}
+	
+	public String processMarkdown() {
+		String retVal = "";
+		try {
+			StringBuffer buff = new StringBuffer();
+			Markdown md = new Markdown();
+			Reader in = new StringReader(this.review);
+			Writer out = new StringBufferWriter(buff);
+			md.transform(in, out);
+			retVal = buff.toString();
+			retVal = HtmlSanitizer.clean(retVal); 
+		} catch (Exception e) {
+			String msg = "Could not parse ActivityResponseReview for markdown '" + 
+						 this.review + "'"; 
+			cLogger.error(msg, e);
+		}
+		return retVal;
 	}
 }
